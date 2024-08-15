5d2c1bc61c1f51b0e37f6b86fd9c192a62eee441
evgeniilisyanskii <evgeniilisyanskii@yandex.ru>
2024-08-15 22:50:56 +1000

    Add files via upload

---
+++ Урок 0.py
@@ -0,0 +1,8 @@
+print ("1st programm")
+print (9 ** 0.5 * 5)
+print ("2st programm")
+print (9.99 > 9.98 and 1000 != 1000.1)
+print ("3st programm")
+print (((2 * 2) + 2) == (2 * (2 + 2)))
+print ("4st programm")
+print (int(float ("123.456") * 10) % 10)
